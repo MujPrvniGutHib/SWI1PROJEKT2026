6ec41cf414f7b78885e7650c94dc4fb8c89e3818
MujPrvniGutHib <xivludvik@gmail.com>
2026-02-23 11:04:40 +0100

    q

--- src/main/java/cz/osu/swicko1backend/controller/MainController.java
+++ src/main/java/cz/osu/swicko1backend/controller/MainController.java
@@ -1,4 +1,18 @@
 package cz.osu.swicko1backend.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class MainController {
+    @Value("${app.name}")
+    private String appName;
+
+
+
+    public String getAppName() {
+        return appName;
+    }
+
 }
