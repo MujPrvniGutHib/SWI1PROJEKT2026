ac3045947f43a6add1ca7dac2e33e0852ad6f216
MujPrvniGutHib <xivludvik@gmail.com>
2026-03-16 10:25:19 +0100

    Update App.tsx

--- src/main/frontend/frontend/src/App.tsx
+++ src/main/frontend/frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Home from './components/Home.tsx'
+import Login from './components/Login.tsx'
+import SignUp from './components/SignUp.tsx'
+
+
 
 function App() {
 
 
   return (
-    <>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+      </Routes>
 
-    </>
+    </div>
   )
 }
 
