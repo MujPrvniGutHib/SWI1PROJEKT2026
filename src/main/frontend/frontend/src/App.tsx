89c54225213ce929bae7d0087c3a02a662368a53
MujPrvniGutHib <xivludvik@gmail.com>
2026-03-16 10:30:17 +0100

    Update App.tsx

--- src/main/frontend/frontend/src/App.tsx
+++ src/main/frontend/frontend/src/App.tsx
@@ -3,11 +3,11 @@ import {Route, Routes} from "react-router-dom";
 import Home from './components/Home.tsx'
 import Login from './components/Login.tsx'
 import SignUp from './components/SignUp.tsx'
-
-
+import {useState} from "react";
 
 function App() {
 
+const[user, setUser] = useState(getUserToken());
 
   return (
     <div className="App">
